a3165f53bab7109050e37fcd63bbc3d5a5f7f5e9
tomcat <tomcat@ip-172-31-3-213.eu-west-1.compute.internal>
2023-01-12 10:58:49 +0000

    Commit all changes from GenMyModel / Architecture Factory

--- src/main/java/net/liccioni/archetypes/uniqueid/UniqueIdentifier.java
+++ src/main/java/net/liccioni/archetypes/uniqueid/UniqueIdentifier.java
@@ -5,4 +5,23 @@ package net.liccioni.archetypes.uniqueid;
 * @generated
 */
 public abstract class UniqueIdentifier {
+
+  /**
+    * @generated
+    */
+    private String identifier;
+
+  /**
+    * @generated
+    */
+    private String getIdentifier() {
+        return this.identifier;
+    }
+
+  /**
+    * @generated
+    */
+    private void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
 }
