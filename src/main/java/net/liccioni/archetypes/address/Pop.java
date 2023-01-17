645175bd7b5e14c79f20ec84a296d9b55e44e69d
tomcat <tomcat@ip-172-31-40-103.eu-west-1.compute.internal>
2023-01-17 10:56:58 +0000

    Commit all changes from GenMyModel / Architecture Factory

---
+++ src/main/java/net/liccioni/archetypes/address/Pop.java
@@ -0,0 +1,34 @@
+package net.liccioni.archetypes.address;
+
+
+/**
+* @generated
+*/
+public class Pop {
+    
+    /**
+    * @generated
+    */
+    private String hello;
+    
+    
+    
+
+    /**
+    * @generated
+    */
+    public String getHello() {
+        return this.hello;
+    }
+    
+    /**
+    * @generated
+    */
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+    
+    
+    
+    
+}
