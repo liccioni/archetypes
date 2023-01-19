837bbba4015eacc24ba32200bdee008e6cd1c9ed
tomcat <tomcat@ip-172-31-40-103.eu-west-1.compute.internal>
2023-01-19 16:06:58 +0000

    Commit all changes from GenMyModel / Architecture Factory

---
+++ src/main/java/net/liccioni/archetypes/product/MeasuredProductInstance.java
@@ -0,0 +1,34 @@
+package net.liccioni.archetypes.product;
+
+
+/**
+* @generated
+*/
+public class MeasuredProductInstance extends ProductInstance {
+    
+    
+    /**
+    * @generated
+    */
+    private Quantity quantity;
+    
+    
+
+    
+    /**
+    * @generated
+    */
+    public Quantity getQuantity() {
+        return this.quantity;
+    }
+    
+    /**
+    * @generated
+    */
+    public void setQuantity(Quantity quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+    
+}
