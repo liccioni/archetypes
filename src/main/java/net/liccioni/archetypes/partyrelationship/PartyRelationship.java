d8d12957845f5d5e9280daf915c646408cddb4e1
tomcat <tomcat@ip-172-31-40-103.eu-west-1.compute.internal>
2023-01-19 09:42:50 +0000

    Commit all changes from GenMyModel / Architecture Factory

---
+++ src/main/java/net/liccioni/archetypes/partyrelationship/PartyRelationship.java
@@ -0,0 +1,94 @@
+package net.liccioni.archetypes.partyrelationship;
+
+
+/**
+* @generated
+*/
+public class PartyRelationship {
+    
+    /**
+    * @generated
+    */
+    private String name;
+    
+    /**
+    * @generated
+    */
+    private String description;
+    
+    
+    /**
+    * @generated
+    */
+    private PartyRole supplier;
+    
+    /**
+    * @generated
+    */
+    private PartyRole client;
+    
+    
+
+    /**
+    * @generated
+    */
+    public String getName() {
+        return this.name;
+    }
+    
+    /**
+    * @generated
+    */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public String getDescription() {
+        return this.description;
+    }
+    
+    /**
+    * @generated
+    */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+    /**
+    * @generated
+    */
+    public PartyRole getSupplier() {
+        return this.supplier;
+    }
+    
+    /**
+    * @generated
+    */
+    public void setSupplier(PartyRole supplier) {
+        this.supplier = supplier;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public PartyRole getClient() {
+        return this.client;
+    }
+    
+    /**
+    * @generated
+    */
+    public void setClient(PartyRole client) {
+        this.client = client;
+    }
+    
+    
+    
+}
