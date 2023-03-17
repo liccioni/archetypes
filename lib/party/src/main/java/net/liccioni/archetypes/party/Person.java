49fdf1c48042c6045fc2642ff1fc044437f802f1
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-03-17 19:20:21 +0100

    fix: adding map struct

--- lib/party/src/main/java/net/liccioni/archetypes/party/Person.java
+++ lib/party/src/main/java/net/liccioni/archetypes/party/Person.java
@@ -1,18 +1,18 @@
 package net.liccioni.archetypes.party;
 
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
+import lombok.Value;
 import lombok.experimental.SuperBuilder;
 
-@Data
+@Value
 @ToString(callSuper = true)
 @SuperBuilder(toBuilder = true)
 @EqualsAndHashCode(callSuper = true)
 public class Person extends Party {
 
     @NonNull
-    private PersonName personName;
+    PersonName personName;
 }
