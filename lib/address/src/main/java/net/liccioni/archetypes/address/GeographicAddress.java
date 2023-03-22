898d2c6e4ea6cd9d353f722da2d79fa3e0bd1a30
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-03-22 13:42:44 +0100

    fix: fixing geographic address

--- lib/address/src/main/java/net/liccioni/archetypes/address/GeographicAddress.java
+++ lib/address/src/main/java/net/liccioni/archetypes/address/GeographicAddress.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
 
@@ -15,12 +16,18 @@ public class GeographicAddress extends Address {
 
     @Builder.Default
     Set<String> addressLine = new HashSet<>();
-    String city;
-    String regionOrState;
-    String zipOrPostCode;
+    @Builder.Default
+    String city = "";
+    @Builder.Default
+    String regionOrState = "";
+    @Builder.Default
+    String zipOrPostCode = "";
+    @NonNull
+    Locale country;
 
     @Override
     public String getAddress() {
-        return this.toString();
+        return String.format("%s, %s, %s, %s, %s",
+                String.join(",", addressLine), city, regionOrState, zipOrPostCode, country.getName());
     }
 }
